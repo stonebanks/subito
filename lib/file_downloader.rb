8d73f01da811ec1e16f2ec37bee2c84402271f85
Allan SEYMOUR <seymour.allan@gmail.com>
2011-11-11 23:40:08 +0100

    creation file_downloader + test

---
+++ lib/file_downloader.rb
@@ -0,0 +1,15 @@
+require 'exception_raiser'
+require 'curb'
+
+
+class FileDownloader
+
+  USER_AGENT = "Mozilla/5.0 (X11; Linux i686; rv:2.0b10)"
+
+  def initialize()
+  end
+
+  def download(str_link, output_name)
+  end
+
+end
