80d38f0ebb34aed28237255e3218ab5979c9a529
Allan Seymour <banks.the.megalithic.stone@gmail.com>
2011-11-12 13:05:15 +0100

    pas grand chose

--- test/file_downloader_test.rb
+++ test/file_downloader_test.rb
@@ -1,1 +1,11 @@
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
 require 'test/unit'
+require 'fakeweb'
+
+
+class FileDownloaderTest < Test::Unit::TestCase
+  def test_should_
+end
+FakeWeb.register_uri(:get, %r|http://example\.com/|, :body => "Hello World!", :content_type => "text/plain")
+
