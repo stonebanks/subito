f864c751ed7ef0b66766b78fd730bd5f59b38ac7
Allan Seymour <banks.the.megalithic.stone@gmail.com>
2012-06-23 14:01:50 +0200

    Downloader : rewriting of retrieve_url_for method

--- lib/subito/downloader.rb
+++ lib/subito/downloader.rb
@@ -4,25 +4,27 @@ require 'subito/browser'
 
 module Subito
   class Downloader
-    attr_accessor :hash,:url,:tv_show_feature
-    def initialize(hash_urls, tv_show_feature)
+    attr_accessor :hash,:url#,:tv_show_feature
+    def initialize(hash_urls)
       @hash = hash_urls
-      @tv_show_feature = tv_show_feature
     end
         
-    def retrieve_url_for(language)
+    def retrieve_url_for(args)
+      args = {:language => nil, :team => nil}.merge(args)
+      team = args[:team]
+      language = args[:language]
       nil
-      team = tv_show_feature.team
-      if hash.has_key? team and hash[team].has_key? language
-        url = SConfig.instance.ressources_subsite_name + hash[team][language].first
-      elsif (real_team = hash.keys.detect {|k| k.include? team})
-        url = SConfig.instance.ressources_subsite_name + hash[real_team][language].first if hash[real_team].has_key? language
-      # elsif team.nil?
-      #   url= SConfig.instance.ressources_subsite_name + hash.values.first.values.flatten!.first unless hash.values.empty? and hash.values.first.values.empty?  
+      if team.nil?
+        scp_level_hash = Array(hash.find {|k,v| v.has_key? language}).last
+        SConfig.instance.ressources_subsite_name + scp_level_hash[language].first unless scp_level_hash.nil? 
+      elsif hash.has_key? team and hash[team].has_key? language
+        SConfig.instance.ressources_subsite_name + hash[team][language].first
+      elsif real_team = hash.keys.detect {|k| k.include? team}
+        SConfig.instance.ressources_subsite_name + hash[real_team][language].first if hash[real_team].has_key? language
       end
     end
 
-    def download(test = @url)
+    def download(test = url)
       Browser.instance.get(test) do |result|
        # filename = tv_show_feature.raw_name[/^(.*\.)[\d\w]{3}$/i,1] + "srt"
         result.save if result.kind_of? Mechanize::File
--- test/downloader_test.rb
+++ test/downloader_test.rb
@@ -10,7 +10,7 @@ include Subito
 class DownloadManagerTest < Test::Unit::TestCase
 
   def setup
-    hash_urls =  {  
+    available_subs =  {  
       "x264-asap" => { 
         "english" => ['/original/1122/0'],
         "french"  => ['/original/21/3'] },
@@ -18,30 +18,28 @@ class DownloadManagerTest < Test::Unit::TestCase
         "english" => ['/original/1223/0'] }
     }
 
-    @tv_show_feature = flexmock("TVShowFeature")
-    @downloader = Downloader.new(hash_urls, @tv_show_feature)
+    # @tv_show_feature = flexmock("TVShowFeature")
+    @downloader = Downloader.new(available_subs)
     flexmock(SConfig).new_instances(:instance).should_receive(:ressources_subsite_name).and_return("http://toto.com")
   end
-  def test_should_retrieve_url_for_the_team_and_language
-    @tv_show_feature.should_receive(:team =>"lol")
+  def test_must_retrieve_url_for_the_given_language_and_team
+  #  @tv_show_feature.should_receive(:team =>"lol")
     assert_equal('http://toto.com/original/1223/0', 
-                 @downloader.retrieve_url_for("english"))
+                 @downloader.retrieve_url_for(language: "english", team: "lol"))
   end
 
-  def test_should_return_nil_if_team_does_not_exist
-    @tv_show_feature.should_receive(:team =>"foo")
-    assert_nil(@downloader.retrieve_url_for("english"))
-  end
-  
-  def test_should_return_nil_if_language_does_not_exist
-    @tv_show_feature.should_receive(:team =>"lol")
-    assert_nil(@downloader.retrieve_url_for("foo"))
-  end
-  
-  def test_should_retrieve_url_if_team_contains_the_given_character
-    @tv_show_feature.should_receive(:team =>"asap")
+  def test_must_retrieve_url_for_the_given_language_but_team_not_complete
     assert_equal('http://toto.com/original/1122/0', 
-                 @downloader.retrieve_url_for("english"))
+                 @downloader.retrieve_url_for(language: "english", team: "asap"))
+  end
+  
+  def test_must_return_the_first_url_found_for_the_given_language_if_team_nil
+     assert_equal('http://toto.com/original/21/3', 
+                 @downloader.retrieve_url_for(language: "french", team: nil))
+  end
+
+  def test_must_return_nil_if_no_language_is_given
+    assert_nil @downloader.retrieve_url_for(team: "hhh")
   end
 end
 
