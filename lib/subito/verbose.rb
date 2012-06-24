75b65aa33fd82362b9cd3e69a801e80aa1365475
SEYMOUR Allan <seymour.allan@gmail.com>
2012-06-24 19:00:03 +0200

    Yet another re write of verbose cause it's better to set attributes after construction

--- lib/subito/verbose.rb
+++ lib/subito/verbose.rb
@@ -2,50 +2,21 @@ $:.unshift File.join(File.dirname(__FILE__), '..')
 require 'logger'
 require 'singleton'
 
-class <<Singleton
-  def included_with_reset(klass)
-    included_without_reset(klass)
-    class <<klass
-      def reset_instance
-        Singleton.send :__init__, self
-        self
-      end
-    end
-  end
-  alias_method :included_without_reset, :included
-  alias_method :included, :included_with_reset
-end
 
 module Subito 
   class Verbose
     include Singleton
-    @@enable
-    @@io
-    @@logger
-    DISPLAY_SETTINGS = [:default, :none]
-    def initialize()
-      @enable, @io, @logger = @@enable, @@io, @@logger
-    end
-    def self.enable=(enable)
-      @@enable = enable
-    end
-    def self.io=(io)
-      @@io = io
-    end 
-    def self.io
-      @@io
-    end
-    def self.logger=(logger)
-      @@logger = logger
+    attr_accessor :enable, :io, :logger
+    
+    def set(enable, io, logger)
+      @enabled, @io, @logger = enable, io, logger
     end
     # outputs a message if message has to be display by default
     # or if Verbose#enable is true in that case  use Logger
-    def msg(message, setting = :none)
+    def msg(message, setting = :default)
       nil
-      @io.puts(message) if setting.eql? :default
-      unless setting.eql? :default or setting.eql? :none
-        @logger.send(setting, message)
-      end
+      @io.puts(message) if not(@enabled) and setting.eql? :default
+      @logger.send(setting.eql?(:default)? :info : setting , message)if @enabled
     end
   end
 end
--- test/verbose_test.rb
+++ test/verbose_test.rb
@@ -4,60 +4,48 @@ require 'subito/verbose'
 
 include Subito 
 class VerboseTest < Test::Unit::TestCase
-  def test_must_output_a_message_if_message_has_to_be_displayed_by_default_and_enable_if_false
+  def test_must_output_a_simple_formatted_message_if_setting_is_default_and_enable_if_false
     r, w = IO.pipe
-    Verbose.enable = false
-    Verbose.io = w
-    Verbose.logger = Logger.new nil
     verbose = Verbose.instance
-    verbose.msg('hello world',:default)
+    verbose.set false, w, Logger.new(nil)
+    verbose.msg('hello world')
     w.close
     ret = r.read
     r.close
-    Verbose.reset_instance
     assert_equal  "hello world\n", ret
   end
   
   def test_must_not_display_anything_if_settings_isnt_default_and_enable_is_false
     r, w = IO.pipe
-    Verbose.enable = false
-    Verbose.io = w
-    Verbose.logger = Logger.new nil
     verbose = Verbose.instance
-    verbose.msg('hello world')
+    verbose.set false, w, Logger.new(nil)
+    verbose.msg('hello world', :info)
     w.close
     ret = r.read
     r.close 
-    Verbose.reset_instance
-    assert_not_equal  "hello world\n", ret
+    assert_equal "", ret
   end
 
 
   def test_must_display_an_info_message_if_enable_is_set_and_setting_is_info
-    r, w = IO.pipe
-    Verbose.enable = true
-    Verbose.io = w
-    Verbose.logger = Logger.new w
+    r, w = IO.pipe  
     verbose = Verbose.instance
+    verbose.set true, w, Logger.new(w)
     verbose.msg('hello world', :info)
     w.close
     ret = r.read
     r.close
-    Verbose.reset_instance
     assert_match %r|^I,\s*\[.*\]\s*INFO.*hello\sworld.*|, ret
   end 
 
   def test_must_display_a_warning_message_if_enable_is_set_and_setting_is_warn
-    r, w = IO.pipe
-    Verbose.enable = true
-    Verbose.io = w
-    Verbose.logger = Logger.new w
+    r, w = IO.pipe 
     verbose = Verbose.instance
+    verbose.set true, w, Logger.new(w)
     verbose.msg('hello world', :warn)
     w.close
     ret = r.read
     r.close
-    Verbose.reset_instance
     assert_match %r|^W,\s*\[.*\]\s*WARN.*hello\sworld.*|, ret
   end
  
