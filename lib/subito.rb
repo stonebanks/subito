a8cb76e7b6a85b8905db2b19f180c91a551ecaf6
SEYMOUR Allan <banks.the.megalithic.stone@gmail.com>
2012-08-02 08:21:32 +0200

    Little correction

--- lib/subito.rb
+++ lib/subito.rb
@@ -14,7 +14,7 @@
 # along with Subito.  If not, see <http://www.gnu.org/licenses/>.
 require 'colored'
 module Subito
-  VERSION = "0.3.0"
+  VERSION = "0.3.1"
   ROOT = File.expand_path(File.dirname(__FILE__))
   Dir.glob(File.join(ROOT, "subito",'*.rb')).reject{|i| i=~/_database/}.
     each  do |file|
